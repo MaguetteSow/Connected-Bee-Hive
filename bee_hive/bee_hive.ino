15f783ac179cc2c3ac4517c06610c6d5b33fe13e
Maguette SOW <72598945+MaguetteSow@users.noreply.github.com>
2022-12-07 09:29:52 +0100

    Add files via upload

--- bee_hive/bee_hive.ino
+++ bee_hive/bee_hive.ino
@@ -1,101 +1,78 @@
 /*
-  First Configuration
+  Connected Bee-Hive Projet | Polytech 2022-2023 | Groupe WEUZ
   This sketch demonstrates the usage of MKR WAN 1300/1310 LoRa module.
-  This example code is in the public domain.
 */
 
-#include <MKRWAN.h>
+
+/* Bibliothéques requises */
 #include <dht.h>
+#include <MKRWAN.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <HX711.h>
 
-#define dht_apin A0 // Analog Pin sensor is connected to
-#define dht_apin22 A1 // Analog Pin sensor is connected to
+/* Déclaration des Pins */
+#define LOADCELL_DOUT_PIN 5
+#define LOADCELL_SCK_PIN 4
+#define dht_apin A0    // Analog Pin sensor is connected to
+#define dht_apin22 A1  // Analog Pin sensor is connected to
 
-dht DHT;
+#define analogPin A3  // Broche à laquelle la batterie se branche
 
-LoRaModem modem;
 
-// Uncomment if using the Murata chip as a module
-// LoRaModem modem(Serial1);
+HX711 scale;      // Capteur de poids
+dht DHT;          // DHT22
+LoRaModem modem;  // modem Lorawan
 
-String appEui;
-String appKey;
-String devAddr;
-String nwkSKey;
-String appSKey;
+//Liaison avec The Things Network
+String appEui = "0000000000000000";
+String appKey = "DB0DD3985A57E1B00C0B1871D5DD9114";
+
+// Pour la lecture des sondes de temp
+OneWire ds18x20[] = { 6, 7 };
+const int oneWireCount = sizeof(ds18x20) / sizeof(OneWire);
+DallasTemperature sensor[oneWireCount];
+
+/*  Variables */
+float calibration_factor = 21067;                                                   // Facteur de calibration capteur poids
+float meas, p, real_meas;                                                           // variables pour caluler le poids
+short temp_sonde1, temp_sonde2, temp_dht, hum_dht, var_poids, var_battery, val_bp;  // pour stocker les valeurs mesurées en 16bits
+
+const byte brocheBouton = 1;  // bouton sur connecteur D2
+const int L1 = 2;             // broche 2 du micro-contrôleur se nomme maintenant : L1
+bool buttonState = false;
+float val = 0;          // variable to store the value read
+float resultat = 0;     // le resultat en volt
+float pourcentage = 0;  //la charge de la batterie en pourcentage
 
 void setup() {
   // put your setup code here, to run once:
-  //Serial.begin(9600);
   Serial.begin(115200);
-  delay(500);//Delay to let system boot
-  Serial.println("DHT11 Humidity & temperature Sensor\n\n");
-  delay(1000);//Wait before accessing Sensor
+  delay(50);  //Delay to let system boot
 
-  pinMode(2,OUTPUT);
+  pinMode(brocheBouton, INPUT);  // configuration en entrée
+  pinMode(L1, OUTPUT);           //L1 est une broche de sortie
 
-  //while (!Serial);  //---on enleve la boucle serial pour voir ce qu'on a 
-  Serial.println("Welcome to MKR WAN 1300/1310 first configuration sketch");
-  Serial.println("Register to your favourite LoRa network and we are ready to go!");
-  // change this to your regional band (eg. US915, AS923, ...)
+  //while (!Serial);  //---on enleve la boucle serial pour voir ce qu'on a
   if (!modem.begin(EU868)) {
     Serial.println("Failed to start module");
     while (1) {}
   };
-  Serial.print("Your module version is: ");
-  Serial.println(modem.version());
+  //Serial.print("Your module version is: ");
+  //Serial.println(modem.version());
   if (modem.version() != ARDUINO_FW_VERSION) {
     Serial.println("Please make sure that the latest modem firmware is installed.");
     Serial.println("To update the firmware upload the 'MKRWANFWUpdate_standalone.ino' sketch.");
   }
-  Serial.print("Your device EUI is: ");
-  Serial.println(modem.deviceEUI());
+  //Serial.print("Your device EUI is: ");
+  //Serial.println(modem.deviceEUI());
 
-  int mode = 0;
-  while (mode != 1 && mode != 2) {
-//    Serial.println("Are you connecting via OTAA (1) or ABP (2)?");
-//    while (!Serial.available());
-//    mode = Serial.readStringUntil('\n').toInt();
-      mode = 1;
-  }
-
+  int mode = 1;
   int connected;
   if (mode == 1) {
-//    Serial.println("Enter your APP EUI");
-//    while (!Serial.available());
-//    appEui = Serial.readStringUntil('\n');
-    //  appEui = 0000000000000000;
-
-
-//    Serial.println("Enter your APP KEY");
-//    while (!Serial.available());
-//    appKey = Serial.readStringUntil('\n');
-    //appKey = DB0DD3985A57E1B00C0B1871D5DD9114;
-
-    appEui = "0000000000000000";
-    appKey = "DB0DD3985A57E1B00C0B1871D5DD9114";
     appKey.trim();
     appEui.trim();
-
     connected = modem.joinOTAA(appEui, appKey);
-  } else if (mode == 2) {
-
-    Serial.println("Enter your Device Address");
-    while (!Serial.available());
-    devAddr = Serial.readStringUntil('\n');
-
-    Serial.println("Enter your NWS KEY");
-    while (!Serial.available());
-    nwkSKey = Serial.readStringUntil('\n');
-
-    Serial.println("Enter your APP SKEY");
-    while (!Serial.available());
-    appSKey = Serial.readStringUntil('\n');
-
-    devAddr.trim();
-    nwkSKey.trim();
-    appSKey.trim();
-
-    connected = modem.joinABP(devAddr, nwkSKey, appSKey);
   }
 
   if (!connected) {
@@ -108,32 +85,93 @@ void setup() {
   int err;
   modem.setPort(3);
   modem.beginPacket();
-  //modem.print("HeLoRA world!");
-  DHT.read11(dht_apin);
+  modem.dataRate(2);
 
-//  modem.print(DHT.humidity,DHT.temperature);
-  //modem.print(DHT.temperature);
-  Serial.print("temperature = ");
-  Serial.print(DHT.temperature);
-  Serial.println("C  ");
-
-  //modem.print(DHT.humidity);
-  Serial.print("Current humidity = ");
-  Serial.print(DHT.humidity);
-  Serial.print("%  ");
-
+  /* Lecture de la température et de l'humidité du DHT11*/
   DHT.read22(dht_apin22);
-  modem.print(DHT.temperature);
   Serial.print("temperature22 = ");
   Serial.print(DHT.temperature);
   Serial.println("C  ");
-  modem.print(DHT.humidity);
+
   Serial.print("Current humidity22 = ");
   Serial.print(DHT.humidity);
   Serial.print("%  ");
-  
+
+  // Multiplication par 100 puis formatage en 16 bits
+  temp_dht = (short)((DHT.temperature) * 100);
+  hum_dht = (short)((DHT.humidity) * 100);
+
+  // Envoie des valeurs
+  //modem.write(temp_dht);
+  modem.write(hum_dht);
+
+  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
+  scale.set_scale();
+  scale.tare();  //Reset the scale to 0
+
+  long zero_factor = scale.read_average();  //Get a baseline reading
+  Serial.print("Zero factor: ");            //This can be used to remove the need to tare the scale. Useful in permanent scale projects.
+  Serial.println(zero_factor);
+
+  meas = scale.get_units(10);
+  //Serial.print(meas);
+  p = meas / 3.7;
+
+  real_meas = meas - p;
+  Serial.print(real_meas);
+  Serial.print(" kg ");  //Change this to kg 
+  var_poids = (short)(real_meas * 100);
+  //modem.print(var_poids);
+
+
+
+  val = analogRead(analogPin);  // read the input pin
+  Serial.print("Val : ");
+  Serial.println(val);
+  resultat = (val * 3.3 / 1023);  //(val*3.3/1024) 10 bits avec analogRead
+  pourcentage = (resultat * 100 / 3.3);
+  Serial.print("Pourcentage batterie : ");  // debug value
+  Serial.println(pourcentage);
+  var_battery = (short)(pourcentage * 100);
+  modem.write(var_battery);
+  Serial.print("Valeur batterie envoyée  : ");
+  Serial.println(var_battery);
+
+  // Start up the library on all defined bus-wires
+  DeviceAddress deviceAddress;
+  for (int i = 0; i < oneWireCount; i++) {
+    sensor[i].setOneWire(&ds18x20[i]);
+    sensor[i].begin();
+    if (sensor[i].getAddress(deviceAddress, 0))
+      sensor[i].setResolution(deviceAddress, 8);
+  }
+
+  /* Lecture de la température des sondes */
+  Serial.print("Requesting temperatures...");
+  for (int i = 0; i < oneWireCount; i++) {
+    sensor[i].requestTemperatures();
+  }
+  Serial.println("DONE");
+  delay(100);
+
+  for (int i = 0; i < oneWireCount; i++) {
+    float temperature = sensor[i].getTempCByIndex(0);
+    Serial.print("Temperature for the sensor ");
+    Serial.print(i + 1);
+    Serial.print(" is ");
+    Serial.println(temperature, 1);
+
+    // Multiplication par 100 puis formatage en 16 bits
+    temp_sonde1 = (short)(temperature * 100);
+    // Envoie des valeurs
+    modem.write(temp_sonde1);
+  }
+  Serial.println();
+
+
+
   err = modem.endPacket(true);
-  if (err > 0) {
+  if (err > 0) {  //En cas d'erreur, indiquer sur moniteur serie
     Serial.println("Message sent correctly!");
   } else {
     Serial.println("Error sending message :(");
@@ -141,25 +179,21 @@ void setup() {
 }
 
 void loop() {
-  //Start of Program
 
-  //Fastest should be once every two seconds.
+  byte etatEntree = digitalRead(brocheBouton);  // retourne 0 (LOW) ou 1 (HIGH)
+  Serial.println(etatEntree);                   // affichage dans Terminal Série
+  //delay(1000); // je mets un temps de delai
+
+  if (etatEntree == HIGH && !buttonState) {
+    digitalWrite(L1, HIGH);      // j'allume ma led
+    delay(1000);                 // je mets un temps de delai
+    digitalWrite(L1, LOW);       // Eteindre L1
+    buttonState = !buttonState;  // je change l'état de false à true
+  }
+  buttonState = etatEntree;
+
   while (modem.available()) {
-//    DHT.read11(dht_apin);
-
-//    Serial.print("Current humidity = ");
-//    Serial.print(DHT.humidity);
-//    Serial.print("%  ");
-//    Serial.print("temperature = ");
-//    Serial.print(DHT.temperature);
-//    Serial.println("C  ");
-
-    //modem.print(DHT.humidity);
     Serial.write(modem.read());
-     digitalWrite(2,HIGH);
   }
   modem.poll();
-
-
-    //delay(5000);//Wait 5 seconds before accessing sensor again.  
 }
